50de4f296d59dc96f4d515ec47f4d183b1a12693
Kundan7634 <1rn21cs083.kundankumar@gmail.com>
2025-02-13 03:16:08 -0800

    initial commit

---
+++ src/test/java/com/example/NammaMetro/Metroservice/MetroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NammaMetro.Metroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
